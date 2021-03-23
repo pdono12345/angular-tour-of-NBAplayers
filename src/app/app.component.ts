2e7c12f9971e7d574ef21278f5e383abbad5d36a
Jovi <pdono12345@gmail.com>
2021-03-23 11:23:03 +0800

    section 0. 建立專案
    - 使用 angular CLI 建立初始應用結構
    
    - 使用 component 顯示資料
    
    - 使用插值(interpolation) 顯示 title
    -- { value }

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-tour-of-heroes';
+  title = '灌籃高手之旅 Tour of SlamDunk';
 }
